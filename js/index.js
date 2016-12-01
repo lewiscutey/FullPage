29a331640da7bcb0afc457e3dc42439fd34405f8
lewiscutey <949124363@qq.com>
2016-12-01 21:55:48 +0800

    'fullpage'

---
+++ js/index.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Lewis on 2016/12/1.
+ */
+$(function () {
+
+   $("#fullpage").fullpage({
+       verticalCentered: false,
+
+   //menu
+       menu: '#menu',
+       anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10'],
+
+   //Navigation
+       lockAnchors: false,
+       navigation: true,
+       navigationTooltips: ['首页', '视觉', '交互', '皮肤', '功能', '待办邮件', '联系人邮件', '科技', '连接易信', '马上体验'],
+       navigationPosition: 'right',
+       showActiveTooltip: false,
+
+   //开始加动画
+       /*afterLoad: function(anchorLink, index){
+           var loadedSection = $(this);
+
+           $(".section").eq(index).addClass("active");
+
+       },*/
+
+   // 离开去动画
+       onLeave: function(index, nextIndex, direction){
+           var leavingSection = $(this);
+
+           $(".section").eq(index).removeClass("active");
+           $("#fullPage-nav ul a").each(function () {
+               $(this).removeClass("active");
+           });
+           $("#fullPage-nav ul a").eq(nextIndex-1).addClass("active");
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   });
+
+
+
+
+
+
+
+
+
+
+});
